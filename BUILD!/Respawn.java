9c4246ed1e42c158ed6f4c642544ce253baffda8
unknown <jodyislami103@gmail.com>
2023-10-24 15:12:28 +0700

    Random Moving Enemy atas Bawah (no comment) + Respawn Enemy (No Comment)

--- BUILD!/Respawn.java
+++ BUILD!/Respawn.java
@@ -13,7 +13,6 @@ public class Respawn {
         this.enemies = enemies;
         this.random = random;
 
-        // Initialize the respawn timer
         respawnTimer = new Timer(2000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -28,18 +27,18 @@ public class Respawn {
     }
 
         public void respawnEnemies() {
-        int numStaticEnemies = random.nextInt(4) + 2; // Randomly generate between 2 and 5 StaticEnemies
-        int numRandomMovingEnemies = random.nextInt(3) + 1; // Randomly generate between 1 and 3 RandomMovingEnemies
+        int numStaticEnemies = random.nextInt(4) + 2; /
+        int numRandomMovingEnemies = random.nextInt(3) + 1; 
 
         for (int i = 0; i < numStaticEnemies; i++) {
-            int xPosition = random.nextInt(700); // Random X position
-            int yPosition = -random.nextInt(100) - 50; // Random Y position above the screen
+            int xPosition = random.nextInt(700); 
+            int yPosition = -random.nextInt(100) - 50; 
             enemies.add(new StaticEnemy(xPosition, yPosition));
         }
 
         for (int i = 0; i < numRandomMovingEnemies; i++) {
-            int xPosition = random.nextInt(700); // Random X position
-            int yPosition = -random.nextInt(100) - 50; // Random Y position above the screen
+            int xPosition = random.nextInt(700); 
+            int yPosition = -random.nextInt(100) - 50; 
             enemies.add(new RandomMovingEnemy(xPosition, yPosition, 1, 0, 800, 1000, random));
         }
     }
